be6398adb76a0fa96074989e3bdc46d769f4f316
Emrullah Karatas <emrullah@efabrika.com>
2017-06-20 10:21:51 +0300

    promise added

--- process.js
+++ process.js
@@ -11,51 +11,65 @@ module.exports = class ImageProcess {
   }
 
   resize(height, width, sourceImage, resultImage) {
-    const command = [
-      'convert',
-      sourceImage,
-      '-resize',
-      `${height}x${width}`,
-      resultImage,
-    ];
-    this.exec(command);
+    return new Promise((resolve) => {
+      const command = [
+        'convert',
+        sourceImage,
+        '-resize',
+        `${height}x${width}`,
+        resultImage,
+      ];
+      this.exec(command)
+        .then(resolve);
+    });
   }
 
   composite(coorX, coorY, imageMain, imageIn, resultImage) {
-    const command = [
-      'composite',
-      '-geometry',
-      `${coorX}${coorY}`,
-      imageIn,
-      imageMain,
-      resultImage,
-    ];
-    this.exec(command);
+    return new Promise((resolve) => {
+      const command = [
+        'composite',
+        '-geometry',
+        `${coorX}${coorY}`,
+        imageIn,
+        imageMain,
+        resultImage,
+      ];
+      this.exec(command)
+        .then(resolve);
+    });
   }
 
   compositeCorner(position, imageMain, imageIn, resultImage) {
-    const command = [
-      'composite',
-      '-gravity',
-      position,
-      imageIn,
-      imageMain,
-      resultImage,
-    ];
-    this.exec(command);
+    return new Promise((resolve) => {
+      const command = [
+        'composite',
+        '-gravity',
+        position,
+        imageIn,
+        imageMain,
+        resultImage,
+      ];
+      this.exec(command)
+        .then(resolve);
+    });
   }
 
-  size(image, cb) {
-    const command = ['identify', '-format', '%wx%h', image];
-    this.exec(command).then((info) => {
-      const width = info.split('x')[0];
-      const height = info.split('x')[1];
-      cb({ width, height });
+  size(image) {
+    return new Promise((resolve) => {
+      const command = ['identify', '-format', '%wx%h', image];
+      this.exec(command).then((info) => {
+        const width = info.split('x')[0];
+        const height = info.split('x')[1];
+        resolve({ width, height });
+      });
     });
   }
 
   rotate(rotate, inputImg, outputImg) {
-    const command = ['convert', inputImg, '-rotate', rotate, outputImg];
-    this.exec(command);
+    return new Promise((resolve) => {
+      const command = ['convert', inputImg, '-rotate', rotate, outputImg];
+      this.exec(command)
+        .then(resolve);
+    });
   }
 };
